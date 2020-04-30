3c4eff38659e7f454a50ec92f666253c169eafcf
Prasad Sawant <prasad.sawant20397@gmail.com>
2020-04-30 12:33:54 +0530

    initial build

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample Newsletter Signup page with MailChimp API
+
+1. Get Api Key and List Id from Mailchimp
+2. Replace API KEY in app.js with your api key and append api url with your list id
+3. Replace X in url in app.js with the last character of your api key
---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request  = require("request");
+const https = require("https");
+
+const app = new express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req,res){
+    //get info from form
+    const info = req.body;
+    const firstName = info.firstName;
+    const lastName = info.lastName;
+    const email = info.email;
+    // console.log(info);
+    //crete oject to send to mailchimp api call
+    const data = {
+        members : [
+            {
+                email_address : email,
+                status : "subscribed",
+                merge_fields : {
+                    FNAME : firstName,
+                    LNAME : lastName
+                }
+            }
+        ]
+    }
+
+    //convert object to string
+    const jsonData = JSON.stringify(data);
+
+    //make request
+    const url  = "https://usX.api.mailchimp.com/3.0/lists/{list_id}}";
+    const options = {
+        method : "POST",
+        auth : "prasad:APIKEY"
+    }
+
+    const request = https.request(url, options, function(response){
+        //redirect based on status code
+        if(response.statusCode == 200){
+            //res.send("Sucessfully subscribed!");
+            res.sendFile(__dirname + "/success.html");
+        } else{
+            //res.send("There was an error with signing up, please try again!");
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
